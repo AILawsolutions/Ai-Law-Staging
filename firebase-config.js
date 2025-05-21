82a22d6982bdccc633118c56c1306efe1b80fb40
AILawsolutions <ailawsolutions@gmail.com>
2025-05-21 11:19:26 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,9 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCJwChpy4l84ojK_M-2zy54iovLGQC9LpQ",
-    authDomain: "ai-law-2.firebaseapp.com",
-    projectId: "ai-law-2",
-    storageBucket: "ai-law-2.firebasestorage.app",
-    messagingSenderId: "599347715793",
-    appId: "1:599347715793:web:ea84d04d421bc94e9baff0",
-    measurementId: "G-NKQX0Y2RR2"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
